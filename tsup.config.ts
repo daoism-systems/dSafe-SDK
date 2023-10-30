742ae911214e141eeac1d445323d97846330fd30
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-10-30 20:20:02 +0530

    feat: add tsup and npm registry config

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  dts: true, // Generate declaration file (.d.ts)
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+});
